253ded183cb3eaa9e3b470de57bb4e26ffe90bd1
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-06-07 15:53:04 +0100

    linked-key-to-audio

---
+++ JavaScript Drum Kit/main.js
@@ -0,0 +1,14 @@
+window.addEventListener("keydown", function(e){
+    //console.log(e);
+    //console.log(e.keyCode);
+    // use attribute selector to grab the data-key attribute audio[data-key]
+    // grab the corresponding audio element for that specific key triggered by that event
+const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`);
+// select corresponding key for annimation
+const key = document.querySelector(`.key[data-key= "${e.keyCode}"]`);
+// console.log(audio);
+if(!audio) return; // stop the function from running if there is no audio found 
+audio.currentTime = 0; // rewind to the start
+audio.play();
+console.log(key);
+});
