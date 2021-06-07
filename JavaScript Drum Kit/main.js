a6a3764d64fbbe15ac09d0ee931af140e1dbde3e
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-06-07 16:34:22 +0100

    added-transition-js

--- JavaScript Drum Kit/main.js
+++ JavaScript Drum Kit/main.js
@@ -1,4 +1,5 @@
-window.addEventListener("keydown", function(e){
+
+function playSound(e){
     //console.log(e);
     //console.log(e.keyCode);
     // use attribute selector to grab the data-key attribute audio[data-key]
@@ -11,4 +12,22 @@ if(!audio) return; // stop the function from running if there is no audio found
 audio.currentTime = 0; // rewind to the start
 audio.play();
 // console.log(key);
-});
+key.classList.add("playing");
+};
+// transition end event - to create transition set in css at the press of the key and remove class playing 
+
+function removeTransition(e){
+// console.log(e);
+if(e.propertyName !== "transform") return; // skip it if it is not a transform
+//console.log(this); this is the key in this case ( what is eventListener is called for)
+// remove class of playing after the 0.07s 
+this.classList.remove("playing");
+}
+// grab all keys with the class of key
+const keys = document.querySelectorAll(".key");
+// listen for the transition event on each of the key part of the key array
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
+
